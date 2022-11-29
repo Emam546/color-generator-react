959d0cdf4f786b63221766e51b5fba789825f7ce
Emam546 <emam54637@gmail.com>
2022-11-29 20:05:36 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,38 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [err,setErr]=useState(false)
+  const [list,setList]=useState(new Values("#f15025").all(10))
+  function handelSubmit(e) {
+    e.preventDefault()
+    const formData=new FormData(e.target)
+    const data=[...formData.values()]
+    try{
+      const colors=new Values(data[0]).all(10)
+      setList(colors)
+      setErr(false)
+    }catch(err){
+      setErr(true)
+    }
+
+  }
+  return <>
+    <section className='container'>
+        <h2>color generator</h2>
+        <form onSubmit={handelSubmit}>
+          <input type="text" className={err?"error":null}/>
+          <button className='btn' type='submit'>
+              submit
+          </button>
+        </form>
+    </section>
+    <section className='colors'>
+        {list.map((color,i)=>{
+          console.log(color);
+          return <SingleColor key={i} color={color} index={i}/>
+        })}
+    </section>
+  </>
 }
 
 export default App
--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,8 +1,26 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({color:{rgb,weight,},index}) => {
+  const hex=rgbToHex(...rgb)
+  const [alert,setAlert]=useState(false)
+  useEffect(()=>{
+    const timeout=setTimeout(()=>{setAlert(false)},2000)
+    return ()=>clearTimeout(timeout)
+  },[alert])
+  function copyToClipBoard(){
+    navigator.clipboard.writeText(hex)
+    setAlert(true)
+    
+  }
+  return <article 
+  className={`color ${index>10&&"color-light"}`}
+  style={{backgroundColor:hex}}
+  onClick={()=>copyToClipBoard()}>
+    <p className='percent-value'>{weight}%</p>
+    <p className='color-value'>{hex}</p>
+    {alert && <p className='alert'>copied to clipboard</p>}
+  </article>
 }
 
 export default SingleColor
